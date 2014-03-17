ecd48840a9db8e2f0fc1b682bf6d07ea2d5b6e11
apsdehal <apsdehal@gmail.com>
2014-03-17 15:56:12 +0530

    Adds table on return

--- assets/js/wikidata.js
+++ assets/js/wikidata.js
@@ -21,15 +21,40 @@ var Api = {
 	}
 }
 
+var General = {
+	createTable: function(){
+		var handle = $(".entities");
+		this.table = $("<table/>");
+		this.table.append($("<tr/>")
+				.append($("<th>").text('id'))
+				.append($("<th>").text('label'))
+				.append($("<th>").text('description'))
+			);
+			handle.append(this.table);
+	},
+
+	addRow: function(object){
+		this.table.append($("<tr>")
+			.append($("<td>").html($("<a href="+object.url+">").text(object.id)))
+			.append($("<td>").text(object.label))
+			.append($("<td>").text(object.description))
+			)
+	}
+};
 
 Api.init('https://www.wikidata.org/w/api.php', 'en');
-console.log(Api.url);
+
 $("#searchbox").bind('keydown', function(e){
 	//Enter key pressed
 	if(e.keyCode == 13){
 		var term = $(this).val();
+
 		Api.searchEntities('item', term).done(function(data){
-			console.log(data);
+			General.createTable();
+			var result = data.search;
+			$.each(result, function(i ,val){
+				General.addRow(val);
+			});
 		});
 	}
 })
